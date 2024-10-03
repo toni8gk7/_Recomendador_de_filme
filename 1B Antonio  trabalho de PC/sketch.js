816711d1a1b9c86974e52a9318a6917d1bcf1527
toni8gk7 <161736678+toni8gk7@users.noreply.github.com>
2024-10-03 09:07:12 -0300

    Update sketch.js

--- 1B Antonio  trabalho de PC/sketch.js
+++ 1B Antonio  trabalho de PC/sketch.js
@@ -6,30 +6,64 @@
 // deadpool & wolverine, 18, ação, comédia
 // gintes de aço, 10, drama, ação, esporte, ficção científica
 // como eu era antes de você , 12, drama, romance
-
-
 let campoIdade;
+let campoFantasia;
+let campoAventura;
+let campoDrama;
 
 function setup() {
-  createCanvas(400, 400);
-  campoIdade = createInput("15");
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+  campoAventura = createCheckbox("Gosta de ação?");
+  campoAventura = createCheckbox("Gosta de drama?");
+  campoAventura = createCheckbox("Gosta de comédia?");
+  campoAventura = createCheckbox("Gosta de animação?");
+  campoAventura = createCheckbox("Gosta de ficção ciêntifica?");
+  campoAventura = createCheckbox("Gosta de romance?");
 }
 
 function draw() {
-  background(220);
-  let idade = 15; // exemplo de idade
-  let recomendacao = geraRecomendacao(idade);
+  background("rgb(10,1,1)");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let gostaDeDrama = campoDrama.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
   text(recomendacao, width / 2, height / 2);
 }
 
-function geraRecomendacao(idade) {
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
   if (idade >= 10) {
     if (idade >= 14) {
       return "O menino que descobriu o vento";
     } else {
-      return "As aventuras de Pi";
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem aranha: no aranhaverso";          
+        } else{
+         return "Ladrões de bicicleta";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "Depois da chuva";
+        }
+      }
     }
   } else {
-    return "A viagem de Chihiro";
+    if (gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
+    }
   }
 }
